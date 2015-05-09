126f9218c168b08b9618ae37b715c0ccd2d7cd5c
AlexKnauth <alexander@knauth.org>
2015-05-08 23:40:35 -0400

    update deps

--- info.rkt
+++ info.rkt
@@ -24,6 +24,8 @@
                      "htdp-lib"  ; why?
                      "pict-doc"
                      "typed-racket-doc"
+                     "images-lib"
+                     "images-doc"
                      ))
 
 (define pkg-desc "Pict3D: Functional 3D Scenes")
